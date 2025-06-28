eb4458d1d59cd81ef37084bc30b134a605cc4a32
M Ifraheem <mifrahim017@gmail.com>
2025-06-29 00:11:21 +0500

    test

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
 print("this is python code in github repo. v2")
 print("this will be auto build in jenkins")
+print("let's see either or not the build show without reloading the page")
+print("let's see either or not the build show without reloading the page")
