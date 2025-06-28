9b22661e2d1d4913fbd9178997011386d541c112
M Ifraheem <mifrahim017@gmail.com>
2025-06-29 00:31:00 +0500

    email test

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print("this is python code in github repo. v2")
 print("this will be auto build in jenkins")
 print("let's see either or not the build show without reloading the page")
 print("let's see either or not the build show without reloading the page")
+print("let's see jenkins can send email notification or not"
+      
